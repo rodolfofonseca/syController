70dd5c29d08d9d0b3a926978de14ac614890b165
Rodolfo Fonseca <rodolfofonseca01@gmail.com>
2024-02-29 00:42:25 -0300

    Desenvolvimento de template
    Desenvolvimento de algumas partes visuais, para em outro momento desenvolver o back e fazer funcionar o que foi proposto

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,8 +1,25 @@
-import {useState } from "react";
+// import { useState } from "react";
+import { articleBody } from "./components/article-body";
+import { narBar } from "./components/nav-bar";
+import { footer } from "./components/footer";
+// import { carousel } from "./components/carousel";
+import { searchArtists } from "./components/search";
+import { login } from "./components/login";
 
 export function App() {
 
   return (
+    <div>
+      {narBar()}
+      {/* {carousel()} */}
 
+      {searchArtists()}
+
+      {articleBody()}
+      {articleBody()}
+      {articleBody()}
+      {footer()}
+      {/* {login()} */}
+    </div>
   )
 }
